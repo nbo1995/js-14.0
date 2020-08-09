43ebdcd1b6766a172087cbe9ec091971f433aac2
Sergey Stepanenko <nbo1995@gmail.com>
2020-08-09 08:33:11 +0400

    I've done my homework2

--- script/script.js
+++ script/script.js
@@ -1,15 +1,27 @@
-let message = function () {
-  alert('Изучил git и github');
-};
+let money = 100000000
+  , income = "100000 руб"
+  , addExpenses = "14000 руб , 600 руб, 4000 руб"
+  , deposit = true
+  , mission = 2000000
+  , period = 12;
 
-let money = "100000000 руб"
-let income = "100000 руб"
-let addExpenses = "30000 руб"
-let deposit = "0 руб"
-let mission = "Научиться верстать полноценные сайты"
-let period = "4 месяца"
+// Вывести в консоль тип данных значений переменных money, income, deposit;
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
 
-console.log(mission);
+// Вывести в консоль длину строки addExpenses
+console.log(addExpenses.length);
 
+// Вывести в консоль “Период равен (period) месяцев” и “Цель заработать (mission) рублей/долларов/гривен/юани”;
+console.log("Период равен " + period + " месяцев");
+console.log("Цель заработать " + mission + " рублей");
 
-document.getElementById('github').addEventListener('click', message);
+// Привести строку addExpenses к нижнему регистру и разбить строку на массив, вывести массив в консоль;
+console.log(addExpenses.toLowerCase().split(', '));
+
+// Объявить переменную budgetDay и присвоить дневной бюджет (доход за месяц / 30);
+let budgetDay = money / 30;
+
+// Вывести в консоль budgetDay
+console.log(budgetDay);
